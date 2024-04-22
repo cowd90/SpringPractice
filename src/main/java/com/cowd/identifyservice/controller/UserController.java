f1cc3c6c18b22cc02601c164e8eba66d0d094012
cowd90 <tienn.dungg.980@gmail.com>
2024-04-23 00:05:17 +0700

    complete crud user

--- src/main/java/com/cowd/identifyservice/controller/UserController.java
+++ src/main/java/com/cowd/identifyservice/controller/UserController.java
@@ -1,21 +1,45 @@
 package com.cowd.identifyservice.controller;
 
 import com.cowd.identifyservice.dto.request.UserCreationRequest;
+import com.cowd.identifyservice.dto.request.UserUpdateRequest;
 import com.cowd.identifyservice.entity.User;
 import com.cowd.identifyservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     User createUser(@RequestBody UserCreationRequest request) {
         return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String id) {
+        return userService.getUser(id);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@RequestBody UserUpdateRequest request,
+                    @PathVariable("userId") String id) {
+        return userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable("userId") String id) {
+        userService.deleteUser(id);
+        return "User has been deleted!";
     }
 
 }
--- src/main/java/com/cowd/identifyservice/service/UserService.java
+++ src/main/java/com/cowd/identifyservice/service/UserService.java
@@ -1,10 +1,13 @@
 package com.cowd.identifyservice.service;
 
 import com.cowd.identifyservice.dto.request.UserCreationRequest;
+import com.cowd.identifyservice.dto.request.UserUpdateRequest;
 import com.cowd.identifyservice.entity.User;
 import com.cowd.identifyservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -20,6 +23,27 @@ public class UserService {
         user.setDob(request.getDob());
 
         return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found!"));
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+        user.setDob(request.getDob());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
     }
 
 }
