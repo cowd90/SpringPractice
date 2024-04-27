ac3cf56f17735d398593f91d478afa0043d99e14
cowd90 <tienn.dungg.980@gmail.com>
2024-04-27 10:24:35 +0700

    exception hanlder, return response with httpcode

--- src/main/java/com/cowd/identifyservice/exception/ErrorCode.java
+++ src/main/java/com/cowd/identifyservice/exception/ErrorCode.java
@@ -1,28 +1,31 @@
 package com.cowd.identifyservice.exception;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error"),
-    INVALID_KEY(1001, "Invalid message key"),
-    USER_EXIST(1002, "User already exists"),
-    USERNAME_INVALID(1003, "Username must be at least 6 characters"),
-    INVALID_PASSWORD(1004, "Password must be at least 8 characters"),
-    USER_NOT_EXISTS(1005, "User not exists."),
-    UNAUTHENTICATED(1006, "Unauthenticated"),
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1001, "Invalid message key", HttpStatus.BAD_REQUEST),
+    USER_EXIST(1002, "User already exists", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least 6 characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTS(1005, "User not exists", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
     ;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
-    private int code;
-    private String message;
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    int code;
+    String message;
+    HttpStatusCode statusCode;
 }
