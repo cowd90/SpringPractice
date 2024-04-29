91359a4b04a5e7bb930fc8477b656c32681fca81
cowd90 <tienn.dungg.980@gmail.com>
2024-04-29 10:53:59 +0700

    update validation with annotation

--- src/main/java/com/cowd/identifyservice/dto/request/UserCreationRequest.java
+++ src/main/java/com/cowd/identifyservice/dto/request/UserCreationRequest.java
@@ -13,16 +13,16 @@ import java.time.LocalDate;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
-    @Size(min = 6, message = "USERNAME_INVALID")
+    @Size(min = 8, message = "USERNAME_INVALID")
     String username;
 
-    @Size(min = 8, message = "INVALID_PASSWORD")
+    @Size(min = 12, message = "INVALID_PASSWORD")
     String password;
 
     String firstName;
 
     String lastName;
 
-    @DobConstraint(min = 18, message = "INVALID_DOB")
+    @DobConstraint(min = 16, message = "INVALID_DOB")
     LocalDate dob;
 }
